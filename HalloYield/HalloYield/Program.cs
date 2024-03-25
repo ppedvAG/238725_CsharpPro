c5b22e4643c5d811bf32d4d4a280b389d2f9d251
André <AndreR@ppedv.de>
2024-03-25 14:29:45 +0100

    Dinge mit Yield

---
+++ HalloYield/HalloYield/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+StreamReader sr = null;
+
+sr.ReadLine();
+
+await foreach (var item in GetZahlen())
+{
+    Console.WriteLine(item);
+}
+
+async IAsyncEnumerable<int> GetZahlen()
+{
+    //var liste = new[] { 1, 2, 3, 4 };
+    //return liste;
+    yield return 1;
+    yield return 2;
+    yield return 3;
+    yield return 4;
+}
+
