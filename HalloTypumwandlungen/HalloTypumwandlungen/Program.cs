ca95c605f8e0b7dabbd3c2876d106dd95da6fae3
André <AndreR@ppedv.de>
2024-03-25 14:29:33 +0100

    Typengedöhns

---
+++ HalloTypumwandlungen/HalloTypumwandlungen/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//object dings = new DateTime(2000, 1, 1);
+object dings = new string("Hallo");
+
+//old style casting
+if (dings != null && dings is DateTime)
+{
+    DateTime dingsAlsDateTime = (DateTime)dings;
+}
+
+//boxing 
+DateTime? dingsAlsDateTimeBoxing = dings as DateTime?;
+if (dingsAlsDateTimeBoxing != null)
+{
+    Console.WriteLine(dingsAlsDateTimeBoxing);
+}
+
+//pattern matching  'is'
+if (dings is DateTime dt)
+{
+    Console.WriteLine(dt);
+}
+    
+
+
+
+
+
+Console.WriteLine("Ende");
