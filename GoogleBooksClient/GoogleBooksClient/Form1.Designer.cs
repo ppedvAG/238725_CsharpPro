33180e802339365dc0119437a71ccec6c04c09e6
André <AndreR@ppedv.de>
2024-03-26 15:13:55 +0100

    Json Serial fun

--- GoogleBooksClient/GoogleBooksClient/Form1.Designer.cs
+++ GoogleBooksClient/GoogleBooksClient/Form1.Designer.cs
@@ -33,8 +33,12 @@
             textBox1 = new TextBox();
             button1 = new Button();
             dataGridView1 = new DataGridView();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            button2 = new Button();
+            button3 = new Button();
             tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            flowLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -53,7 +57,7 @@
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 1;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tableLayoutPanel1.Size = new Size(1560, 48);
+            tableLayoutPanel1.Size = new Size(1211, 48);
             tableLayoutPanel1.TabIndex = 0;
             // 
             // label1
@@ -71,7 +75,7 @@
             textBox1.Anchor = AnchorStyles.Left | AnchorStyles.Right;
             textBox1.Location = new Point(93, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(1355, 39);
+            textBox1.Size = new Size(1006, 39);
             textBox1.TabIndex = 1;
             textBox1.Text = "Bier";
             // 
@@ -79,7 +83,7 @@
             // 
             button1.AutoSize = true;
             button1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            button1.Location = new Point(1454, 3);
+            button1.Location = new Point(1105, 3);
             button1.Name = "button1";
             button1.Size = new Size(103, 42);
             button1.TabIndex = 2;
@@ -91,18 +95,56 @@
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Dock = DockStyle.Fill;
-            dataGridView1.Location = new Point(0, 48);
+            dataGridView1.Location = new Point(0, 96);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 62;
-            dataGridView1.Size = new Size(1560, 528);
+            dataGridView1.Size = new Size(1211, 480);
             dataGridView1.TabIndex = 1;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.AutoSize = true;
+            flowLayoutPanel1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            flowLayoutPanel1.BackColor = Color.ForestGreen;
+            flowLayoutPanel1.Controls.Add(button2);
+            flowLayoutPanel1.Controls.Add(button3);
+            flowLayoutPanel1.Dock = DockStyle.Top;
+            flowLayoutPanel1.Location = new Point(0, 48);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(1211, 48);
+            flowLayoutPanel1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            button2.AutoSize = true;
+            button2.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            button2.Location = new Point(3, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(216, 42);
+            button2.TabIndex = 0;
+            button2.Text = "💾Save as JSON...";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.AutoSize = true;
+            button3.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            button3.Location = new Point(225, 3);
+            button3.Name = "button3";
+            button3.Size = new Size(224, 42);
+            button3.TabIndex = 1;
+            button3.Text = "📂 Load as JSON...";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1560, 576);
+            ClientSize = new Size(1211, 576);
             Controls.Add(dataGridView1);
+            Controls.Add(flowLayoutPanel1);
             Controls.Add(tableLayoutPanel1);
             Font = new Font("Segoe UI", 12F);
             Margin = new Padding(4);
@@ -111,6 +153,8 @@
             tableLayoutPanel1.ResumeLayout(false);
             tableLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -122,5 +166,8 @@
         private TextBox textBox1;
         private Button button1;
         private DataGridView dataGridView1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button button2;
+        private Button button3;
     }
 }
--- GoogleBooksClient/GoogleBooksClient/Form1.cs
+++ GoogleBooksClient/GoogleBooksClient/Form1.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System.Text.Json;
 
 namespace GoogleBooksClient
@@ -18,6 +19,39 @@ namespace GoogleBooksClient
             BooksResult result = JsonSerializer.Deserialize<BooksResult>(json);
 
             dataGridView1.DataSource = result.items.Select(x => x.volumeInfo).ToList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var dlg = new SaveFileDialog()
+            {
+                Filter = "JSON-Datei|*.json;*.jsn|Irgendeine Datei|*.*"
+            };
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                var options = new JsonSerializerOptions
+                {
+                    Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                    WriteIndented = true // Für besser lesbare Ausgabe
+                };
+                var json = JsonSerializer.Serialize(dataGridView1.DataSource, options);
+                File.WriteAllText(dlg.FileName, json);
+            }
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            var dlg = new OpenFileDialog()
+            {
+                Filter = "JSON-Datei|*.json;*.jsn|Irgendeine Datei|*.*"
+            };
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                var json = await File.ReadAllTextAsync(dlg.FileName);
+                dataGridView1.DataSource = JsonSerializer.Deserialize<IEnumerable<Volumeinfo>>(json);
+            }
         }
     }
 }
